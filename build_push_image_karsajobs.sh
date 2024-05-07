a47c7ccadf394835e98d51ec1a36158515c95f14
Wildan <wildanfajriyann@gmail.com>
2024-05-07 18:57:08 +0700

    initial commit

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,18 @@
+# mendefinisikan variabel
+username="wildanfajriyan"
+
+# membuat image
+echo -e "Build new Docker image:"
+docker build . -t $username/karsajobs:latest
+
+# menampilkan daftar image
+echo -e "\nList of Docker images:"
+docker images
+
+# masuk ke Docker hub
+echo -e "\nLogin Docker hub"
+echo $PASSWORD_DOCKER_HUB | docker login -u $username --password-stdin
+
+# mengunggah image ke Docker hub
+echo -e "\nPush image to Docker hub"
+docker push $username/karsajobs:latest
